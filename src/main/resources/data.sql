628098c44796914b4e7b1ed3a84199cb5d8e2927
KeeYoungYoon <44644430+KeeYoungYoon@users.noreply.github.com>
2019-08-09 00:26:51 +0900

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,69 +1,109 @@
-insert into svc_table values(1,'유니세프 정기 후원','2019-02-23','2019-01-23',3,200000,400000,1,'하나은행','123-1230-12343','https://www.unicef.or.kr');
+insert into svc_table values(1,'유니세프 정기후원','2019-02-23','2019-01-23',3,200000,400000,1,'하나은행','123-1230-12343','https://www.unicef.or.kr/join/regular_support.asp');
 insert into svc_table values(
 	2,
-	'두번째 후원 제목',
+	'초록 우산 어린이 재단 정기후원',
 	'2019-03-23',
 	'2019-02-23',
 	3,
-	800000000,
-	800000000,
+	8000000,
+	8000000,
 	4,
-	'HANA',
+	'하나은행',
 	'345-3453-23423',
-	'https://www.sc.or.kr/'	
+	'https://support.childfund.or.kr/regularSpon/regular.do'	
 );
 insert into svc_table values(
 	3,
-	'세번째 후원 제목',
+	'그린피스 정기 / 일시후원',
 	'2019-06-23',
 	'2019-05-23',
 	4,
-	200000000,
-	600000000,
+	24500000,
+	60000000,
 	8,
-	'HAVA',
+	'하나은행',
 	'456-3233-23563',
-	'https://www.sc.or.kr/'	
+	'https://donate.greenpeace.or.kr/'	
 );
 insert into svc_table values(
 	4,
-	'네번째 후원 제목',
+	'밀알 복지 재단 정기후원',
 	'2019-07-23',
 	'2019-03-23',
 	2,
-	50000000,
-	100000000,
+	5280000,
+	10000000,
 	2,
-	'HAVA',
+	'하나은행',
 	'123-456-547456',
-	'https://www.worldvision.or.kr/index.asp'	
+	'http://www.miral.org/donate/regular.asp'	
 );
 insert into svc_table values(
 	5,
-	'다섯번째 후원 제목',
+	'세이브 더 칠드런 정기후원',
 	'2019-11-23',
 	'2019-03-23',
 	6,
 	300000,
 	300000,
 	8,
-	'HAVA',
+	'하나은행',
 	'245-6456-76433',
-	'https://www.sc.or.kr/'	
+	'https://www.sc.or.kr/support/supportMonth.do'	
 );
 insert into svc_table values(
 	6,
-	'여섯번째 후원 제목',
+	'국경 없는 의사회 정기후원',
 	'2019-12-23',
 	'2019-01-23',
 	6,
 	400000,
 	400000,
 	44,
-	'HAVA',
+	'하나은행',
 	'654-5423-245743',
-	'https://www.worldvision.or.kr/index.asp'	
+	'https://www.msf.or.kr/monthly'	
 );
+insert into svc_table values(
+	7,
+	'굿피플 정기후원',
+	'2019-12-23',
+	'2019-01-23',
+	6,
+	676000,
+	700000,
+	44,
+	'하나은행',
+	'654-5423-245743',
+	'https://www.goodpeople.or.kr/sponsor/regular_sponsor.php'	
+);
+insert into svc_table values(
+	8,
+	'월드비전 정기후원',
+	'2019-12-23',
+	'2019-01-23',
+	6,
+	323000,
+	800000,
+	44,
+	'하나은행',
+	'654-5423-245743',
+	'https://m.worldvision.or.kr/hope/sponsorship_select.asp'	
+);
+insert into svc_table values(
+	9,
+	'굿네이버스 정기후원',
+	'2019-12-23',
+	'2019-01-23',
+	6,
+	542300,
+	700000,
+	44,
+	'하나은행',
+	'654-5423-245743',
+	'https://www.goodneighbors.kr/support_pay/regular.gn'	
+);
+
 insert into svc_comments values(
 	default,
 	1,
